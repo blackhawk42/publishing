cdb2881eed4cbb38000799b232d50103ba9381e1
Erick de Jesus Hernandez Cerecedo <erickdejesushernandezcerecedo@MacBook-Pro-de-Erick-468.local>
2016-09-08 09:52:37 -0500

    agregado magazine

--- Magazine.java
+++ Magazine.java
@@ -1,4 +1,42 @@
-public class Magazine
-{
-	
+public class Magazine{
+    
+    
+    private String title;
+    private String volume;
+    private int year;
+    
+    public magazine(){
+        this.title = "";
+        this.volume="";
+        this.year=0;
+        
+    }
+    
+    public magazine(String title,  String volume, int year){
+        this.title=title;
+        this.volume=volume;
+        this.year=year;
+        
+    }
+    
+    public String getTitle(){
+        return this.title;
+        
+    }
+    
+    public void setTitle(String title){
+        this.title=title;
+    }
+    
+    public String getVolume(){
+        return this.volume;
+        
+    }
+    
+    public void setVolume(String volume){
+        this.volume=volume;
+        
+    }
+    
+    
 }
