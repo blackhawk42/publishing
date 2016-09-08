284f627d13b9b74400899e0af3ef1beb62239847
Erick de Jesus Hernandez Cerecedo <erickcerecedo@gmail.com>
2016-09-08 16:57:21 -0500

    class Magazine, me base en la clase book

--- Magazine.java
+++ Magazine.java
@@ -1,42 +1,124 @@
+/**
+ * Magazine class
+ * This class implements a magazine.
+ *
+ *@author Daniel Ch·vez and Erick H Cerecedo
+ *@version 1.0
+ **/
+
 public class Magazine{
     
     
-    private String title;
-    private String volume;
-    private int year;
+    private String title;// Title
+    private String volume;// Author
+    private int year;// Year of publicacion
     
-    public magazine(){
+    /**
+     * Blank constructor
+     */
+    
+    public Magazine(){
         this.title = "";
         this.volume="";
         this.year=0;
         
     }
     
-    public magazine(String title,  String volume, int year){
+    
+    /**
+     * Non-blank constructor
+     *
+     *@param title Title of the magazine
+     *@param volume Number of publicacion
+     *@param year numero de publicacion.
+     **/
+    
+    public Magazine(String title,  String volume, int year){
         this.title=title;
         this.volume=volume;
         this.year=year;
         
     }
     
+    /**
+     *Accessor for the title attribute
+     *
+     *@return Current title of the magazine
+     */
+    
     public String getTitle(){
         return this.title;
         
     }
     
-    public void setTitle(String title){
-        this.title=title;
-    }
+    /**
+     *Accessor for the volume attribute
+     *
+     *@return Current volume of the magazine
+     */
     
     public String getVolume(){
         return this.volume;
         
     }
     
-    public void setVolume(String volume){
-        this.volume=volume;
+    /**
+     *Accessor for the year attribute
+     *
+     *@return Current year of the magazine
+     */
+    
+    public int getYear(){
+        return this.year;
         
     }
+    
+    /**
+     * Mutator for the title attribute
+     *
+     * @param	title	New string for the title
+     */
+    
+    public void setTitle(String title)
+    {
+        this.title = title;
+        
+    }
+    
+    /**
+     * Mutator for the volume attribute
+     *
+     * @param volume	New string for the volume
+     */
+    
+    public void setVolume(String volume)
+    {
+        this.volume = volume;
+        
+    }
+    
+    /**
+     *Mutator for the year of publicacion attribute
+     *
+     *@param year NEw int for the year of publiction
+     **/
+    
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+    
+    
+    /**
+     * Stringify method
+     * 
+     * @return	Stringified version of the magazine
+     */
+    public String toString()
+    {
+        return this.title + ", number " + this.volume + " (" + this.year + " year)";
+    }
+    
     
     
 }
