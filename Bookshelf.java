2e3f03a7953a9982a83342ea16ad6e5a64b0a65a
Daniel Chávez <danielsaul96@gmail.com>
2016-09-11 00:13:22 -0500

    Bookshelf. Inicia la remodelación

--- Bookshelf.java
+++ Bookshelf.java
@@ -1,36 +1,18 @@
 /**
  * Bookshelf class
  * This class implements a bookshelf.
+ * <p>
+ * It can contain exactly 3 objects of the class Book and 3 objects of
+ * the class Magazine, per homework instructions. This, of course,
+ * could be expanded.
  * 
  * @author Daniel Chávez y and Erick H Cerecedo
  * @version 1.0
  */
 public class Bookshelf
 {
-        
-        public static void main(String[] args){
-            
-            Magazine magazineBlank = new Magazine();
-            System.out.println(magazineBlank.toString());
-            
-            Magazine magazine1 = new Magazine("People", "9", 2016);
-            Magazine magazine2 = new Magazine("Vogue", "9", 2016);
-            Magazine magazine3 = new Magazine("PLAYBOY", "9", 2016);
-            
-            System.out.println(magazine1.toString());
-            System.out.println(magazine2.toString());
-            System.out.println(magazine3.toString());
-            
-            Book bookBlank = new Book();
-            System.out.println(bookBlank.toString());
-            
-            Book book1 = new Book("MAZE RUNNER","James Dashner",400);
-            Book book2 = new Book("INQUEBRANTABLE","Laura Hillenbrand",585);
-            Book book3 = new Book("REFLEXIONES FILOSOFICAS","Francisco Diaz Estrada",182);
-            
-            System.out.println(book1.toString());
-            System.out.println(book2.toString());
-            System.out.println(book3.toString());
-            
-        }
+	private Book[] books;
+	private Magazine[] magazines;
+	
+	public Bookshelf
 }
